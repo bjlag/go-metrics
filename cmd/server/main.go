6ae8ac86eaa68b32846e01a4f8cb9615887d7b87
Vladislav Duplin <vaduplin@avito.ru>
2024-09-02 19:41:26 +0300

    iter1: Remove duplicates

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,16 +19,10 @@ func main() {
 }
 
 func run() error {
-	// todo middleware на проверку метода на POST
-	// todo middleware проверка заголовка Content-Type: text/plain
-	// todo валидатор http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
-	// todo валидация значения метрики gauge float64, counter int64
-	// todo редиректы не поддерживаются
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/update/", updateCommonHandler)
-	mux.HandleFunc("/update/gauge/", updateGaugeHandler)
-	mux.HandleFunc("/update/counter/", updateCounterHandler)
+	mux.Handle("/update/gauge/", makeUpdateHandler(updateGaugeHandler))
+	mux.Handle("/update/counter/", makeUpdateHandler(updateCounterHandler))
 
 	log.Printf("Listening on %s", port)
 
@@ -56,63 +50,46 @@ func updateCommonHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, invalidMetricTypeMsgErr, http.StatusBadRequest)
 }
 
-func updateGaugeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-
-	if withoutNamePattern.MatchString(r.URL.Path) {
-		http.Error(w, noNameMetricMsgErr, http.StatusNotFound)
-		return
-	}
-
-	m := validRoutePattern.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.Error(w, invalidMetricPathMsgErr, http.StatusBadRequest)
-		return
-	}
-
-	typeMetric := m[1]
-	nameMetric := m[2]
-	valueMetric, err := strconv.ParseFloat(m[3], 64)
+func updateGaugeHandler(w http.ResponseWriter, r *http.Request, nameMetric, valueMetric string) {
+	value, err := strconv.ParseFloat(valueMetric, 64)
 	if err != nil {
 		http.Error(w, invalidTypeValueMsgErr, http.StatusBadRequest)
 		return
 	}
 
-	_, _ = fmt.Fprintf(w, "type: %s\n", typeMetric)
-	_, _ = fmt.Fprintf(w, "name: %s\n", nameMetric)
-	_, _ = fmt.Fprintf(w, "value: %v\n", valueMetric)
+	_ = value
 }
 
-func updateCounterHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-
-	if withoutNamePattern.MatchString(r.URL.Path) {
-		http.Error(w, noNameMetricMsgErr, http.StatusNotFound)
-		return
-	}
-
-	m := validRoutePattern.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.Error(w, invalidMetricPathMsgErr, http.StatusBadRequest)
-		return
-	}
-
-	typeMetric := m[1]
-	nameMetric := m[2]
-	valueMetric, err := strconv.ParseInt(m[3], 10, 64)
+func updateCounterHandler(w http.ResponseWriter, r *http.Request, nameMetric, valueMetric string) {
+	value, err := strconv.ParseInt(valueMetric, 10, 64)
 	if err != nil {
 		http.Error(w, invalidTypeValueMsgErr, http.StatusBadRequest)
 		return
 	}
 
-	_, _ = fmt.Fprintf(w, "type: %s\n", typeMetric)
-	_, _ = fmt.Fprintf(w, "name: %s\n", nameMetric)
-	_, _ = fmt.Fprintf(w, "value: %v\n", valueMetric)
+	_ = value
+}
+
+func makeUpdateHandler(handler func(w http.ResponseWriter, r *http.Request, name, value string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+			return
+		}
+
+		if withoutNamePattern.MatchString(r.URL.Path) {
+			http.Error(w, noNameMetricMsgErr, http.StatusNotFound)
+			return
+		}
+
+		m := validRoutePattern.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.Error(w, invalidMetricPathMsgErr, http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Metric received: type '%s', name '%s', value '%s'\n", m[1], m[2], m[3])
+
+		handler(w, r, m[2], m[3])
+	}
 }
