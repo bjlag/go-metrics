fdc440221fc2ffaae19af6e5ed5bcb1492a52899
Vladislav Duplin <vaduplin@avito.ru>
2024-09-25 10:11:54 +0300

    iter7: Tests

--- internal/agent/client/client_test.go
+++ internal/agent/client/client_test.go
@@ -1,6 +1,8 @@
 package client_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
@@ -11,6 +13,7 @@ import (
 
 	"github.com/bjlag/go-metrics/internal/agent/client"
 	"github.com/bjlag/go-metrics/internal/agent/collector"
+	"github.com/bjlag/go-metrics/internal/model"
 )
 
 func TestMetricSender_Send(t *testing.T) {
@@ -19,33 +22,113 @@ func TestMetricSender_Send(t *testing.T) {
 	}
 
 	tests := []struct {
-		name string
-		args args
+		name       string
+		args       args
+		server     *httptest.Server
+		wantStatus int
+		wantErr    bool
 	}{
 		{
-			name: "success",
+			name: "success counter",
 			args: args{
-				metric: collector.NewMetric("kind", "name", 1),
+				metric: collector.NewMetric("counter", "name", 1),
 			},
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/update/", r.URL.Path)
+				assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+				assert.Equal(t, "POST", r.Method)
+
+				var err error
+				var buf bytes.Buffer
+
+				_, err = buf.ReadFrom(r.Body)
+				assert.NoError(t, err)
+
+				in := &model.UpdateIn{}
+				err = json.Unmarshal(buf.Bytes(), in)
+				assert.NoError(t, err)
+
+				assert.Equal(t, "name", in.ID)
+				assert.Equal(t, "counter", in.MType)
+				assert.Equal(t, int64(1), *in.Delta)
+				assert.Nil(t, in.Value)
+			})),
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "success gauge",
+			args: args{
+				metric: collector.NewMetric("gauge", "name", 1.1),
+			},
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/update/", r.URL.Path)
+				assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+				assert.Equal(t, "POST", r.Method)
+
+				var err error
+				var buf bytes.Buffer
+
+				_, err = buf.ReadFrom(r.Body)
+				assert.NoError(t, err)
+
+				in := &model.UpdateIn{}
+				err = json.Unmarshal(buf.Bytes(), in)
+				assert.NoError(t, err)
+
+				assert.Equal(t, "name", in.ID)
+				assert.Equal(t, "gauge", in.MType)
+				assert.Equal(t, 1.1, *in.Value)
+				assert.Nil(t, in.Delta)
+			})),
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "error unknown metric type",
+			args: args{
+				metric: collector.NewMetric("unknown_type", "name", 1.1),
+			},
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/update/", r.URL.Path)
+				assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+				assert.Equal(t, "POST", r.Method)
+
+				var err error
+				var buf bytes.Buffer
+
+				_, err = buf.ReadFrom(r.Body)
+				assert.NoError(t, err)
+
+				in := &model.UpdateIn{}
+				err = json.Unmarshal(buf.Bytes(), in)
+				assert.NoError(t, err)
+
+				assert.Equal(t, "name", in.ID)
+				assert.Equal(t, "gauge", in.MType)
+				assert.Equal(t, 1.1, *in.Value)
+				assert.Nil(t, in.Delta)
+			})),
+			wantErr: true,
 		},
 	}
 
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/update/kind/name/1", r.URL.Path)
-		assert.Equal(t, "text/plain", r.Header.Get("Content-Type"))
-	}))
-	defer server.Close()
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			parts := strings.Split(server.Listener.Addr().String(), ":")
+			defer tt.server.Close()
+
+			parts := strings.Split(tt.server.Listener.Addr().String(), ":")
 			port, _ := strconv.Atoi(parts[1])
 
 			c := client.NewHTTPSender(parts[0], port)
 
-			got, _ := c.Send(tt.args.metric)
+			got, err := c.Send(tt.args.metric)
 
-			assert.Equal(t, got.StatusCode(), http.StatusOK)
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Nil(t, got)
+				return
+			}
+
+			assert.Equal(t, tt.wantStatus, got.StatusCode())
 		})
 	}
 }
