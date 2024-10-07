a3a7ba9417382e6d25ab3c28a6d156a5593a3380
Vladislav Duplin <vaduplin@avito.ru>
2024-10-07 19:40:03 +0300

    iter10: Fix HTTP response status

--- internal/handler/value/general/handler.go
+++ internal/handler/value/general/handler.go
@@ -30,7 +30,7 @@ func (h Handler) Handle(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		h.log.WithField("error", err.Error()).
 			Error("Error reading request body")
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
@@ -40,13 +40,13 @@ func (h Handler) Handle(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		h.log.WithField("error", err.Error()).
 			Error("Unmarshal error")
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	if in.ID == "" {
 		h.log.Info("Metric ID not specified")
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
